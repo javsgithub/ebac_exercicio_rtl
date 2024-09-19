80c984c4eff8687e5af4b6cd8c3bea5edeafecce
Jefferson Santos <doctorjeffbr@yahoo.com.br>
2024-09-19 11:34:14 -0300

    inserindo novo código para novos testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,24 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+    it('Deve inserir o comentário "Uau!! Que miniatura irada!!"', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: "Uau!! Que miniatura irada!!"
+            }        
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+        expect(screen.getByText('Uau!! Que miniatura irada!!')).toBeInTheDocument();
+    });
+    it('Deve inserir o comentário "Maneiro, já tive uma igual!!"', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: "Maneiro, já tive uma igual!!"
+            }        
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+        expect(screen.getByText('Maneiro, já tive uma igual!!')).toBeInTheDocument();
+    });
+})
